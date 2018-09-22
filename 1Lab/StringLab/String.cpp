9a33032152fdf6b94dbfa3477601443cd407d78a
Ivan <medvedev.id@bk.ru>
2018-09-23 00:47:26 +0300

    Add some methods. 100%

--- 1Lab/StringLab/String.cpp
+++ 1Lab/StringLab/String.cpp
@@ -25,7 +25,16 @@ int String::Length()
     return str_length;
 }
 
-//COPY
+void String::Copy(const String& str)
+{
+    str_length=str.str_length;
+    delete [] str_adr;
+    this->str_adr = new char[str_length+1];
+    for(int i=0;i<str.str_length;i++)
+    {
+        this->str_adr[i]=str.str_adr[i];
+    }
+}
 
 int String::Find(char ch, int start)
 {
@@ -41,10 +50,105 @@ int String::Find(char ch, int start)
 int String::FindLast(char ch)
 {
     int i=str_length-1;
-    while (str_adr[i]!=str_adr[0])
+    while (str_adr[i]!=str_adr[-1])
     {
         if (str_adr[i]==ch) {return i;}
         i--;
     }
     return -1;
+}
+
+String String::Substr(int index,int count)//со
+{
+    char *buf = new char[str_length];
+    int j=0;
+    for( int i=index;i<index+count;i++)
+    {
+        buf[j]=str_adr[i];
+        j++;
+    }
+    return buf;
+}
+
+void String::Remove(int index, int count)
+{
+    char *buffer = new char[str_length+1];
+    int j=0;
+    for(int i=0;i<this->Length();i++)
+    {
+        if(i<index || i>=index+count)
+        {
+        buffer[j]=str_adr[i];
+        j++;
+        }
+    }
+    String::Copy(buffer);
+    delete [] buffer;
+}
+
+void String::Insert(char *s, int index)
+{
+    char *buffer = new char[50];
+    int j=0;
+
+    for(int i=0;i<this->Length();i++)
+    {
+        if(i==index)
+        {
+            int counter_while=i;
+            if(index==0)
+            {
+                while(str_adr[j] != '\0')
+                {
+                buffer[j]=str_adr[j];
+                j++;
+                }
+                j=0;
+                while(s[j]!='\0')
+                {
+                str_adr[counter_while]=s[j];
+                counter_while++;
+                j++;
+                str_length++;
+                }
+                while(*buffer!='\0')
+                {
+                str_adr[counter_while]=*buffer;
+                counter_while++;
+                buffer++;
+            }
+            break;
+            }
+
+            else
+            {
+            while(s[j] != '\0')
+            {
+                buffer[j]=str_adr[counter_while];
+                str_adr[counter_while]=s[j];
+                counter_while++;
+                j++;
+                str_length++;
+            }
+            while(*buffer!='\0')
+            {
+                str_adr[counter_while]=*buffer;
+                counter_while++;
+                buffer++;
+            }
+            break;
+            }
+        }
+    }
+    delete [] buffer;
+}
+
+
+
+void String::print()
+{
+    for(int i=0;i<this->Length();i++)
+    {
+        printf("%c" ,this->str_adr[i]);
+    }
 }
--- 1Lab/StringLab/main.cpp
+++ 1Lab/StringLab/main.cpp
@@ -6,9 +6,15 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL,"Russian");
-    cout << "Please, enter a string: ";
     char *p_arr=new char[50];
+    char *p_arr2=new char[50];
+    char *p_arr3 = new char[50];
+    cout << "Пожалуйста, введите первую строку: ";
     gets(p_arr);
+    cout << "Пожалуйста, введите copy-строку: ";
+    gets(p_arr2);
+    cout << "Пожалуйста, введите insert-строку: ";
+    gets(p_arr3);
     int dlina=0;
     cout << "-----------------------"<< endl;
 
@@ -16,10 +22,16 @@ int main()
     String *stroka=new String(p_arr);//Конструктор для объекта строка(с параметрами)
     dlina = stroka->Length();//Присвоение переменной длины строки
     printf("Длина строки: %d", dlina);//Вывод длины строки
-    cout << endl << "-----------------------"<< endl;
+    cout << endl << "-----------------------" << endl;
 
 
     //---------------COPY FUNCTION
+    String *stroka2=new String(p_arr2);
+    stroka->Copy(*stroka2);
+    cout << "Рузльтат метода Copy: ";
+    stroka->print();
+    cout << endl << "-----------------------" << endl;
+    //---------------
 
     //---------------FIND FUNCTION
     char sym;
@@ -28,7 +40,7 @@ int main()
     cin >> sym;
     cout << "Введите с какого индекса искать: ";
     cin >> start_pos;
-    while(start_pos>=stroka->Length() || start_pos<0)//Доработать isdigit()
+    while(start_pos>=stroka->Length() || start_pos<0)
     {
         cout << "Введите индекс корректно: ";
         cin >> start_pos;
@@ -47,14 +59,72 @@ int main()
     cout << endl << "-----------------------" << endl;
     //---------------
 
+    //---------------SUBSTR FUNCTION
+    String *Sub_str = new String[50];
+    int index_sub;
+    int count_sub;
+    cout << "Введите индекс, с которого будет начато выделение строки: ";
+    cin >> index_sub;
+     while(index_sub<0 || index_sub>=stroka->Length())
+    {
+        cout << "Введите индекс корректно: ";
+        cin >> index_sub;
+    }
+    cout << "Введите длину, выделяемой подстроки: ";
+    cin >> count_sub;
+    while(count_sub>stroka->Length()-index_sub || count_sub<=0)
+    {
+        cout << "Введите длину корректно: ";
+        cin >> count_sub;
+    }
+    *Sub_str=stroka->Substr(index_sub,count_sub);
+    printf("Выделенная строка методом Substr: ");
+    Sub_str->print();
+    delete [] Sub_str;
+    cout << endl << "-----------------------" << endl;
+    //---------------
 
+    //---------------REMOVE FUNCTION
+    int index_remove,count_remove;
+    cout << "Введите индекс: ";
+    cin >> index_remove;
+    while(index_remove<0 || index_remove>=stroka->Length())
+    {
+        cout << "Введите индекс корректно: ";
+        cin >> index_remove;
+    }
+    cout << "Введите длину удаляемой строки: ";
+    cin >> count_remove;
+    while(count_remove<0 || count_remove>stroka->Length()-index_remove)
+    {
+        cout << "Введите длину удаляемой строки корректно: ";
+        cin >> count_remove;
+    }
+    stroka->Remove(index_remove,count_remove);
+    cout << "Рузльтат метода Remove: ";
+    stroka->print();
+    cout << endl << "-----------------------" << endl;
 
-
-
-
+    //---------------INSERT FUNCTION
+    int index_insert=0;
+    cout << "Введите индекс, перед которым будем вставлена строка методом Insert: ";
+    cin >> index_insert;
+    while(index_insert<0 || index_insert>stroka->Length())
+    {
+        cout << "Введите индекс корректно: ";
+        cin >> index_insert;
+    }
+    stroka->Insert(p_arr3,index_insert);
+    cout << "Результат выполнения метода Insert: ";
+    stroka->print();
+    cout << endl << "-----------------------" << endl;
+    //---------------
 
 
     delete stroka;
     delete [] p_arr;
+    delete stroka2;
+    delete [] p_arr2;
+    delete [] p_arr3;
     return 0;
 }
